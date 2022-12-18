69fb3cee17a5c01b3509da94741229e7d76b6477
akrck02 <aketza.vazquez@gmail.com>
2022-12-19 00:06:52 +0100

    [code] Adding test scene for button mechanics

---
+++ IoToolkit/Assets/Scripts/Toolkit/core/mechanic/Engines/ButtonClick.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour
+{
+
+    private readonly bool DEFAULT_STARTED = false;
+    public bool started; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.started = DEFAULT_STARTED;   
+    }
+
+
+    public void activate() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+}
